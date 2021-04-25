85c4f58fefc0846722494941b37a1009bb8896fa
JoshuaHolloway <jhollow6@asu.edu>
2021-04-24 20:03:24 -0500

    mounted lifecycle hook

---
+++ 005-event-2/main.js
@@ -0,0 +1,19 @@
+const app = new Vue({
+  el: '#root',  // surface area
+
+  data: {
+    message: 'Hello World!',
+    coordinates: ['x', 'y', 'z']
+  },
+
+  mounted() {
+    const button = document.querySelector('button');
+    button.addEventListener('click', () => {
+      console.log('clicked');
+
+      let name = document.querySelector('#input').value;
+      app.coordinates.push(name);
+    });
+  },
+  
+});
