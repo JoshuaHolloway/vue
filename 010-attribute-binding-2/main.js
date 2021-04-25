a8f21d02a0de92c9712f0ca5ea94dee07cf28f66
JoshuaHolloway <jhollow6@asu.edu>
2021-04-25 10:52:14 -0500

    attribute binding shorthand with :attribute

---
+++ 010-attribute-binding-2/main.js
@@ -0,0 +1,11 @@
+const app = new Vue({
+  
+  // mount Vue here:
+  el: '#root',  // surface area
+
+  // data-binding:
+  data: {
+    the_title: 'This is being set by the vue instances data property...'
+  },
+
+});
