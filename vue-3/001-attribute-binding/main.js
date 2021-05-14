0f65d32efad5d85fcd602971c4fc87d596dbad4f
JoshuaHolloway <jhollow6@asu.edu>
2021-05-14 10:48:06 -0500

    attribute binding

---
+++ vue-3/001-attribute-binding/main.js
@@ -0,0 +1,13 @@
+const options_obj = {
+  data() {
+    return {
+      product: 'Socks',
+      description: 'This is description...',
+      image: './assets/img/socks.png',
+      url: 'https://www.google.com'
+    };
+  }
+};
+
+// Create Vue instance
+const app = Vue.createApp(options_obj);
